cccbd5e4dce37f1e4d6209c6a4b7bcc52af732a8
Jennifer Fernández <j.fdezmurga@gmail.com>
2024-10-10 08:42:59 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
+import "./assets/img/gatotriste.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["Mi hijo", "El gato", "Mi perro", "Un mirlo negro"];
+  let action = ["mojó", "desmontó", "rompió", "saboteó"];
+  let what = ["mi portátil", "mi móvil", "el cable del wifi"];
+  let when = [
+    "mientras dormía.",
+    "cuando fuí por el pan.",
+    "en un arrebato de ira.",
+    "cuando estaba en la ducha."
+  ];
+
+  function generateExcuse() {
+    let randomwho = Math.floor(Math.random() * who.length);
+    let randomaction = Math.floor(Math.random() * action.length);
+    let randomwhat = Math.floor(Math.random() * what.length);
+    let randomwhen = Math.floor(Math.random() * when.length);
+
+    let excuse =
+      who[randomwho] +
+      " " +
+      action[randomaction] +
+      " " +
+      what[randomwhat] +
+      " " +
+      when[randomwhen];
+
+    return excuse;
+  }
+  document.getElementById("excuse").innerText = generateExcuse();
 };
